79497b05c0ce3938766128abe70f9b0a863979ce
s_nishida <s_nishida@four-e.co.jp>
2018-07-30 14:16:30 +0900

    HttpURLConnection/XmlPullParser

---
+++ app/src/main/java/com/example/nishida/booksearch/Common.java
@@ -0,0 +1,8 @@
+package com.example.nishida.booksearch;
+
+public class Common {
+    static final String SERVICE_API_URL = "http://iss.ndl.go.jp/api/opensearch?title=";
+    static final int RESULT_SUCCESS = 0;
+    static final int RESULT_ERROR= 1;
+    static final int RESULT_USER_EXISTS = 2;
+}
--- app/src/main/java/com/example/nishida/booksearch/MainActivity.java
+++ app/src/main/java/com/example/nishida/booksearch/MainActivity.java
@@ -11,13 +11,35 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressDialog m_ProgressDialog;
     ArrayAdapter<String> adapter;
     String inputTxt;
+    String res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +81,140 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    //ToDo
     public void btn_Click(View v) {
 
-        Toast.makeText(getApplicationContext(), inputTxt, Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), inputTxt, Toast.LENGTH_LONG).show();
+        if (inputTxt==null){
+            inputTxt = "";
+        }
+        new TaskBookSearch().execute(inputTxt);
 
     }
 
+    public class TaskBookSearch extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            m_ProgressDialog = ProgressDialog.show(MainActivity.this, "しばらくお待ち下さい", "検索処理中...", true);
+        }
 
+        String retTxt;
+
+        @Override
+        protected Integer doInBackground(String... params) {
+
+            //Map<String, String> param = new HashMap<>();
+
+            try {
+
+                // 取得したテキストを格納する変数
+                final StringBuilder result = new StringBuilder();
+                // アクセス先URL
+                final URL url = new URL(Common.SERVICE_API_URL + params[0]);
+
+                HttpURLConnection con = null;
+                try {
+                    // ローカル処理
+                    // コネクション取得
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();
+
+                    // HTTPレスポンスコード
+                    final int status = con.getResponseCode();
+                    if (status == HttpURLConnection.HTTP_OK) {
+                        // 通信に成功した
+                        // テキストを取得する
+                        final InputStream in = con.getInputStream();
+                        //final String encoding = con.getContentEncoding();
+                        //final InputStreamReader inReader = new InputStreamReader(in, encoding);
+                        final InputStreamReader inReader = new InputStreamReader(in);
+                        final BufferedReader bufReader = new BufferedReader(inReader);
+                        String line = null;
+                        // 1行ずつテキストを読み込む
+                        while((line = bufReader.readLine()) != null) {
+                            result.append(line);
+                        }
+
+                        bufReader.close();
+                        inReader.close();
+                        in.close();
+                    }
+
+                } catch (MalformedURLException e1) {
+                    e1.printStackTrace();
+                } catch (ProtocolException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        // コネクションを切断
+                        con.disconnect();
+                    }
+                }
+
+                res = result.toString();
+
+                return 1;
+            } catch (Exception e) {
+                return Common.RESULT_ERROR;
+            }
+        }
+
+
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            m_ProgressDialog.dismiss();
+            /*
+            if(Common.RESULT_SUCCESS == result) {
+
+            } else {
+
+            }
+            */
+
+            try{
+                parseXml(res);
+            }catch (Exception e){
+
+            }
+
+        }
+
+        private void parseXml(String xmlStr) throws XmlPullParserException, IOException {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(xmlStr));
+
+            int eventType = parser.getEventType();
+            String elementName;
+
+            setContentView(R.layout.activity_main);
+            ListView lv = (ListView)findViewById(R.id.listViewBooks);
+            ArrayList<String> arrayBooks = new ArrayList<>();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    elementName = parser.getName();
+                    if(elementName.equals("title")){
+                        eventType = parser.next();
+                        if(eventType == XmlPullParser.TEXT){
+                            arrayBooks.add(parser.getText());
+                        }
+                    }
+                }
+                eventType = parser.next();
+            }
+
+
+            adapter = new ArrayAdapter<>(
+                    MainActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    arrayBooks);
+            lv.setAdapter(adapter);
+        }
+    }
 }
