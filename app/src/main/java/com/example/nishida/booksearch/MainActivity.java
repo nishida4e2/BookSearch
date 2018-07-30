a7efa608c98020cdee6f13c07da66efb252adc11
s_nishida <s_nishida@four-e.co.jp>
2018-07-30 13:26:21 +0900

    ListView

--- app/src/main/java/com/example/nishida/booksearch/MainActivity.java
+++ app/src/main/java/com/example/nishida/booksearch/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.nishida.booksearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView)findViewById(R.id.listViewBooks);
+        ArrayList<String> arrayBooks = new ArrayList<>();
+        arrayBooks.addAll(Arrays.asList(getResources().getStringArray(R.array.array_test)));
+
+        adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayBooks);
+        lv.setAdapter(adapter);
+
     }
 }
