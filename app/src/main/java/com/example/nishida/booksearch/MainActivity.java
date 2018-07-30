2333b17a2cc904f60172780192b8120c106cfc44
s_nishida <s_nishida@four-e.co.jp>
2018-07-31 00:33:35 +0900

    refac

--- app/src/main/java/com/example/nishida/booksearch/MainActivity.java
+++ app/src/main/java/com/example/nishida/booksearch/MainActivity.java
@@ -49,44 +49,21 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_search, menu);
         MenuItem item = menu.findItem(R.id.menuSearch);
         SearchView searchView = (SearchView)item.getActionView();
-
         searchView.setSubmitButtonEnabled(true);
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                if (inputTxt==null){
-                    inputTxt = "";
-                }
-
+                if (inputTxt==null)inputTxt = "";
                 new TaskBookSearch().execute(inputTxt);
-
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String newText) {
                 inputTxt = newText;
                 return false;
             }
         });
-
         return super.onCreateOptionsMenu(menu);
-    }
-
-    //ToDo
-    public void btn_Click(View v) {
-
-        //Toast.makeText(getApplicationContext(), inputTxt, Toast.LENGTH_LONG).show();
-        if (inputTxt==null){
-            inputTxt = "";
-        }
-
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-
-        new TaskBookSearch().execute(inputTxt);
-
     }
 
     public class TaskBookSearch extends AsyncTask<String, Void, Integer> {
@@ -95,110 +72,69 @@ public class MainActivity extends AppCompatActivity {
             super.onPreExecute();
             m_ProgressDialog = ProgressDialog.show(MainActivity.this, "しばらくお待ちください", "検索中...", true);
         }
-
-        String retTxt;
-
         @Override
         protected Integer doInBackground(String... params) {
-
-            //Map<String, String> param = new HashMap<>();
-
             try {
-
-                // 取得したテキストを格納する変数
                 final StringBuilder result = new StringBuilder();
-                // アクセス先URL
                 final URL url = new URL(Common.SERVICE_API_URL + params[0]);
-
                 HttpURLConnection con = null;
                 try {
-                    // ローカル処理
-                    // コネクション取得
                     con = (HttpURLConnection) url.openConnection();
                     con.connect();
-
-                    // HTTPレスポンスコード
                     final int status = con.getResponseCode();
                     if (status == HttpURLConnection.HTTP_OK) {
-                        // 通信に成功した
-                        // テキストを取得する
                         final InputStream in = con.getInputStream();
-                        //final String encoding = con.getContentEncoding();
-                        //final InputStreamReader inReader = new InputStreamReader(in, encoding);
                         final InputStreamReader inReader = new InputStreamReader(in);
                         final BufferedReader bufReader = new BufferedReader(inReader);
                         String line = null;
-                        // 1行ずつテキストを読み込む
                         while((line = bufReader.readLine()) != null) {
                             result.append(line);
                         }
-
                         bufReader.close();
                         inReader.close();
                         in.close();
                     }
-
-                } catch (MalformedURLException e1) {
-                    e1.printStackTrace();
-                } catch (ProtocolException e1) {
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 } finally {
-                    if (con != null) {
-                        // コネクションを切断
-                        con.disconnect();
-                    }
+                    if (con != null) con.disconnect();
                 }
-
                 retXml = result.toString();
-
-                return 1;
+                return Common.RESULT_SUCCESS;
             } catch (Exception e) {
+                e.printStackTrace();
                 return Common.RESULT_ERROR;
             }
         }
-
-
 
         @Override
         protected void onPostExecute(Integer result) {
             super.onPostExecute(result);
             m_ProgressDialog.dismiss();
-            /*
-            if(Common.RESULT_SUCCESS == result) {
-
-            } else {
-
-            }
-            */
-
-            if ("".equals(retXml)){
+            if (Common.RESULT_ERROR == result || "".equals(retXml)){
                 Toast.makeText(getApplicationContext(), "インターネットに接続されていません。接続を確認してから再度お試しください。", Toast.LENGTH_LONG).show();
             }
-
             try{
                 parseXml(retXml);
             }catch (Exception e){
-
+                e.printStackTrace();
             }
-
         }
 
         private void parseXml(String xmlStr) throws XmlPullParserException, IOException {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser parser = factory.newPullParser();
             parser.setInput(new StringReader(xmlStr));
-
             int eventType = parser.getEventType();
             String elementName;
-
             setContentView(R.layout.activity_main);
             ListView lv = (ListView)findViewById(R.id.listViewBooks);
             ArrayList<String> arrayBooks = new ArrayList<>();
-
             int cnt = 0;
-
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 if (eventType == XmlPullParser.START_TAG) {
                     elementName = parser.getName();
@@ -212,12 +148,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 eventType = parser.next();
             }
-
             if(cnt==1){
-                //arrayBooks.add("検索ワード「"+inputTxt+"」に一致する書籍は見つかりませんでした。別の検索ワードをお試しください。");
                 Toast.makeText(getApplicationContext(), "検索ワード「"+inputTxt+"」に一致する書籍は見つかりませんでした。別の検索ワードをお試しください。", Toast.LENGTH_LONG).show();
             }
-
 
             adapter = new ArrayAdapter<>(
                     MainActivity.this,
